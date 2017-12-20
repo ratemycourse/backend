d345bdc33decd65affaf083b4581f79227652f3f
Johannes Carlsten <jcarlste@kth.se>
2017-12-20 11:51:18 +0100

    Signed-off-by: Johannes Carlsten <jcarlste@kth.se>

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const xml = require('xml')
 
 const connection = mysql.createConnection({
   host: 'localhost',
