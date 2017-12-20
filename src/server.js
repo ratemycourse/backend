4e4504d3334265a650feb3a03e72ee70b9c677c5
Johannes Carlsten <jcarlste@kth.se>
2017-12-20 10:44:17 +0100

    Signed-off-by: Johannes Carlsten <jcarlste@kth.se>

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'test',
+  password: 'cocacola',
+  database: 'ratemycourseDB',
+});
+
+connection.connect();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM dd', (err, result) => {
+    if (err) { console.log(err); }
+    console.log(result);
+    res.json(result);
+  });
+});
+
+app.listen(3000, () => console.log('server API listening on port 3000!'));
